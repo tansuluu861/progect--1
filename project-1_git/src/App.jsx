3606cdfebb2ba98f539d25f1d3d21ae28ea447e1
tansuluu861 <tansuluujanishbekova2@gmail.com>
2025-04-16 21:01:30 +0600

    fix commit

--- project-1_git/src/App.jsx
+++ project-1_git/src/App.jsx
@@ -12,11 +12,16 @@ function App() {
     setCount((prev) => prev - 1);
   };
 
+  const reset = () => {
+    setCount (0)
+  }
+
   return (
     <div>
     <h1>{count}</h1>
     <button onClick={increment}>increment</button>
     <button onClick={decrement}>decrement</button>
+    <button onClick={reset}>reset</button>
     </div>
   );
 }
